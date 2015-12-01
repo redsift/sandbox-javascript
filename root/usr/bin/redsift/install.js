57eafeb320bca5636136d066e8f32ded93f06607
Rahul Powar <rahul@redsift.io>
2015-12-01 19:06:24 +0000

    Fixed for install process

--- root/usr/bin/redsift/install.js
+++ root/usr/bin/redsift/install.js
@@ -1,4 +1,16 @@
+/* global process */
 var npm = require('npm');
+var SERVER = 'server';
+
+try {
+	process.chdir(SERVER);
+}
+catch (err) {
+	console.warn('Could not change directory to', SERVER, ', likely no server implementation');
+	// not an error
+	process.exit(0);
+}
+
 npm.load(function (err) {
 	// catch errors
 	npm.commands.install([ ], function (err, data) {
